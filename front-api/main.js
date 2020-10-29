a624803d289c2bed7116186985eb67e8dc872d80
Walter Osorio <wrosorio@indracompany.com>
2020-10-29 18:27:43 -0500

    Add users

--- front-api/main.js
+++ front-api/main.js
@@ -1,21 +1,67 @@
-const loadTweets=()=>{
-   
+const { DocumentProvider } = require("mongoose");
 
-    const url='https://twitter-walrog.herokuapp.com/api/tweets';
+const loadTweets = () => {
+
+
+    const url = 'https://twitter-walrog.herokuapp.com/api/tweets';
     fetch(url)
-    .then(response =>response.json())
-    .then(json=>{
+        .then(response => response.json())
+        .then(json => {
 
-        
 
-            const tweets  = json.items[0].tweets;
-            let texto =[];
-            tweets.map(t=>texto.push(t.user.name+"<br/>"+t.text+"<br>"+t.createdAt));
+
+            const tweets = json.items[0].tweets;
+            let texto = [];
+            tweets.map(t => texto.push(t.user.name + "<br/>" + t.text + "<br>" + t.createdAt));
             document.getElementById('tweets').innerHTML = texto;
 
-        
 
-    })
-    .catch(error=> alert('No se pudo cargar los datos error:'+error));
-    
+
+        })
+        .catch(error => alert('No se pudo cargar los datos error:' + error));
+
 }
+
+
+async function postData(url = '', data = {}) {
+    // Opciones por defecto estan marcadas con un *
+    const response = await fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+}
+
+
+
+const createUser = () => {
+    const url = 'https://twitter-walrog.herokuapp.com/api/users/';
+
+    var dataUser = {
+        name: document.getElementById('name').value,
+        username: document.getElementById('uname').value,
+        email: document.getElementById('email').value,
+        password: document.getElementById('psw').value,
+        passwordConfirmation: document.getElementById('pswr').value
+    };
+
+
+    postData(url, dataUser)
+        .then(data => {
+            
+            alert("El usuario "+data.username+" se guardo correctamente!"); // JSON data parsed by `data.json()` call
+        })
+        .catch(error => alert('No se pudo guardar el usuario:' + error));
+
+
+}
+
